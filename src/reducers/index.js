bd734bc46ed35ec8c615543532384f689e3b3e40
Yuumi <yuuming.n@gmail.com>
2018-10-17 01:05:36 -0700

    Create index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
+import booksReducer from './reducer_books';
 
 const rootReducer = combineReducers({
+  books: booksReducer
 });
 
 export default rootReducer;
